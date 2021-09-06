86e968ab31e2393ed9f0bd12aa529e7b665a2489
Juan Pablo Agudelo Florez <juan.ablo1999@gmail.com>
2021-09-06 09:54:05 -0500

    Cambios para producción v3

--- src/routes/indexRoutes.js
+++ src/routes/indexRoutes.js
@@ -15,9 +15,6 @@ router.post('/changePassword', usersController.changePassword)
 router.post('/updateUser', usersController.updateUser)
 router.get('/getUser', usersController.getOneUser)
 router.get('/getAllUsers', usersController.getUsers)
-router.get('/', (req,res) => {
-    res.send("service connect.");
-} )
 //rutas de reservas
 router.get('/getReservas', reservasController.getAllReservas)
 router.post('/newReserva', reservasController.saveReserva)
